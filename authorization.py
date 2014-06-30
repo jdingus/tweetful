4ff9bfcecdb5b040db5d3a5246d96c40b542f594
Josh <josh.dingus@gmail.com>
2014-06-29 21:33:24 -0400

    Successful at retrieving twitter timeline

--- authorization.py
+++ authorization.py
@@ -27,8 +27,31 @@ def get_user_authorization(request_token):
     print 'Please go here and authorize: ' + authorize_url
     return raw_input('Please input the verifier: ')
 
+def get_access_token(request_token, request_secret, verifier):
+    """"
+    Get a token which will allow us to make requests to the API
+    """
+    oauth = OAuth1(CLIENT_KEY,
+                   client_secret=CLIENT_SECRET,
+                   resource_owner_key=request_token,
+                   resource_owner_secret=request_secret,
+                   verifier=verifier)
+
+    response = requests.post(ACCESS_TOKEN_URL, auth=oauth)
+    credentials = urlparse.parse_qs(response.content)
+    access_token = credentials.get('oauth_token')[0]
+    access_secret = credentials.get('oauth_token_secret')[0]
+    return access_token, access_secret
+
 def authorize():
 	""" A complete OAuth authentication flow """
 	request_token, request_secret = get_request_token()
 	verifier = get_user_authorization(request_token)
-	# print request_token, request_secret
+	access_token, access_secret = get_access_token(request_token,
+	                                                   request_secret,         
+	                                                   verifier)
+   	oauth = OAuth1(CLIENT_KEY,
+                   	client_secret=CLIENT_SECRET,
+                   	resource_owner_key=access_token,
+                   	resource_owner_secret=access_secret)
+	return oauth
--- tweetful.py
+++ tweetful.py
@@ -1,9 +1,13 @@
 import authorization
-
+import requests
+from urls import TIMELINE_URL
 
 def main():
 	""" Main function """
-	authorization.authorize()
+	oauth = authorization.authorize()
+	response = requests.get(TIMELINE_URL, auth=oauth)
+	print response.json()
+
 
 if __name__ == "__main__":
 	main()
