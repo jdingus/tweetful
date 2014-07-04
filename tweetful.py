79c47c45d63efdd86a0351a067f6ab24ff9bade5
Josh <josh.dingus@gmail.com>
2014-07-04 13:29:55 -0400

    get_trends function working

--- tweetful.py
+++ tweetful.py
@@ -1,46 +1,89 @@
 import authorization
 import requests
-
 import json
+import logging
+import argparse
+import sys # Access argv variable
+from urls import * # Location of endpoints for TwitterAPI
+from collections import OrderedDict # Used in JSON printing
 
-from urls import TIMELINE_URL
-# import json
+# Set the log output file, and the log level
+logging.basicConfig(filename="output.log", level=logging.INFO)
+
+def print_json_file(json_file):
+	""" 
+	Pass json_file as string and will pretty print json
+	"""
+	import pprint # alternative used to print unicode files
+
+	with open(json_file) as json_file:
+		json_data = json.load(json_file, object_pairs_hook=OrderedDict)
+
+	print json.dumps(json_data, indent=2)
+
+	print "\n"
+	print "-----" * 4
+	print "\n"
+
+def tweet(tw_text):
+	pass
+	""" Perform a tweet using TwitterAPI based on passed tw_text """
+	logging.info("Attempting to tweet '{}'".format(tw_text))
+
+def get_trends(woeid):
+	"""
+	Get singular trends response from TwitterAPI and store to trends.json
+	vars, woeid	
+	Go here to look up a WOEID:
+	http://woeid.rosselliot.co.nz/
+	"""
+	# Chicago, Cook County 			WOEID: 2379574
+	# Indianapolis, Marion County 	WOEID: 2427032
+	
+	oauth = authorization.authorize()
+	query = "?id="+str(woeid)
+	url = TRENDS_URL+query
+	response = requests.get(url, auth=oauth)
+	response = response.json()
+	
+	with open('trends.json', 'w') as outfile: # Save to file
+		json.dump(response, outfile)
+
+	""" Parse out the 'names' from response and return names[] """
+	trends = parse_trends('trends.json')
+
+	return woeid,trends
+
+def parse_trends(json_file):
+	""" Parse out the 'names' from response and return trends[] """
+	trends = []
+	with open(json_file) as json_file:
+		json_data = json.load(json_file)
+
+		for data in json_data:
+			for item in json_data[0]['trends']:
+				trend_name = item['name']
+				trends.append(trend_name)
+	return trends
+
 
 def main():
 	""" Main function """
-	oauth = authorization.authorize()
-#	response = requests.get('https://api.twitter.com/1.1/trends/place.json?id=1', auth=oauth)
-	# response = requests.get(TIMELINE_URL, auth=oauth)
-#	new_response = response.json()
 
-	
-#	with open('data.txt', 'w') as outfile:
- # 		json.dump(new_response, outfile) # Save json to file for debugging
+	woeid = 2379574 # Chicago
+	# woeid = 2427032 # Indy
+	get_trends(woeid)
 
-	with open("data.txt") as json_file:
-    		json_data = json.load(json_file)
- 	   
-    	# print json_data[0].items()
-    	print json_data[0][u'trends'][2]  #[u'name']
-    	for name in json_data[0][u'trends'][2]:
-    		print json_data[0][u'trends'][2][name]
-    	# 	print json_data[0][u'trends'][item][u'name'] 
-
-
-
-
-	# # for key in new_response:
-	# # new_response[u'trends']
-	# print new_response
-	# # [0][u'trends'][u'urls']
-	# print ''
-	# # for o in new_response:
-	# 	print ''
-	# 	print '*******************************'
-	# 	print ''
-	# print new_response[0][u'locations']        #,o['favorite_count']
-# Fort Wayne, Indiana (US-IN) WOEID: 2406008
-# Fort Wayne, Indiana (US-IN) WOEID: 56559321	
+	"""
+	Steps:
+		1.) Construct make_parser() >> -tweet , -trends
+		2.) Get Trends from Twitter by WOEID#
+			> If one item passed will just return that item
+			> If multiple WOEID's pass will return both and compare
+			> Store resulting data to trends.json
+		3.) Perform status update, aka tweet to my user account
+		4.) Make sure to do commits along the way and use error logging
+	"""
 
 if __name__ == "__main__":
 	main()
